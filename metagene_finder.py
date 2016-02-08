73d39ed4b7a174499aaa239b02154cc77cd84335
Oliver Steele <steele@osteele.com>
2016-02-08 11:40:16 -0500

    dynamic programming

--- metagene_finder.py
+++ metagene_finder.py
@@ -8,59 +8,33 @@ SoftDes 2016 Mini Project 1: Gene Finder
 
 from load import load_nitrogenase_seq, load_metagenome
 
-def make_substring_tree(string):
-    """Return a tree of substrings of string. The tree is represented as a rooted graph
-    whose nodes are dicts and whose keys are letters of the string.
-
-    Examples:
-        >>> make_substring_tree('abc')
-        {'a': {'b': {'c': {}}}, 'c': {}, 'b': {'c': {}}}
-    """
-    def extend_graph(node, string):
-        for edge in string:
-            child = node.get(edge)
-            if not child:
-                node[edge] = child = {}
-            node = child
-
-    root = {}
-    for i in xrange(len(string)):
-        extend_graph(root, string[i:])
-    return root
-
-def find_longest_substring_length(string, substring_tree):
+def find_longest_common_substring_length(s1, s2):
     """Return the length of the longest substring of `string` in `substring_set_array`,
     where `substring_set_array` is a value returned by `make_substring_set_array`.
 
     Examples:
-        >>> t = make_substring_tree('abc')
-        >>> find_longest_substring_length('xaz', t)
+        >>> find_longest_common_substring_length('abc', 'xaz')
         1
-        >>> find_longest_substring_length('xabz', t)
+        >>> find_longest_common_substring_length('abc', 'xabz')
         2
-        >>> find_longest_substring_length('xabcz', t)
+        >>> find_longest_common_substring_length('abc', 'xabcz')
         3
-        >>> find_longest_substring_length('xyabcz', t)
+        >>> find_longest_common_substring_length('abc', 'xyabcz')
         3
-        >>> find_longest_substring_length('abcz', t)
+        >>> find_longest_common_substring_length('abc', 'abcz')
         3
-        >>> find_longest_substring_length('xyabc', t)
+        >>> find_longest_common_substring_length('abc', 'xyabc')
         3
-        >>> find_longest_substring_length('xyz', t)
+        >>> find_longest_common_substring_length('abc', 'xyz')
         0
     """
 
-    longest = 0
-    for i in xrange(len(string)):
-        if longest > len(string) - i + 1:
-            break
-        node = substring_tree
-        for j in xrange(i, len(string)):
-            node = node.get(string[j])
-            if node is None:
-                break
-            longest = max(longest, j + 1 - i)
-    return longest
+    L = {}
+    for i, c1 in enumerate(s1):
+        for j, c2 in enumerate(s2):
+            if c1 == c2:
+                L[i, j] = L.get((i - 1, j - 1), 0) + 1
+    return max(L.values() or [0])
 
 def find_snippet_with_greatest_overlap(target_sequence, snippets):
     """Return the name of the snippet whose sequence has the greatest overlap with `target_sequence`.
@@ -70,15 +44,13 @@ def find_snippet_with_greatest_overlap(target_sequence, snippets):
         target_sequence (str): the nucleotide sequence
 
     Examples:
-        >>> t = make_substring_tree('TAG')
         >>> snippets = [('1', 'AG'), ('2', 'AGAGAG'), ('3', 'ATAGA')]
         >>> find_snippet_with_greatest_overlap('TAG', snippets)
         '3'
         >>> find_snippet_with_greatest_overlap('AGAG', snippets)
         '2'
     """
-    substring_tree = make_substring_tree(target_sequence)
-    snippet_name, _ = max(snippets, key=lambda snippet: find_longest_substring_length(snippet[1], substring_tree))
+    snippet_name, _ = max(snippets, key=lambda snippet: find_longest_common_substring_length(snippet[1], target_sequence))
     return snippet_name
 
 
